fb455b96b9dd64e592655b3f525cfd5b3d2dafb8
Stormlight <Littletexan06@icloud.com>
2026-06-15 16:58:55 -0500

    Update SmartQuest boot script for v2

--- src/ServerScriptService/SmartQuestService/SmartQuestServerMain.server.lua
+++ src/ServerScriptService/SmartQuestService/SmartQuestServerMain.server.lua
@@ -1,9 +1,22 @@
 --!strict
 -- SmartQuestServerMain
--- Simple boot script for SmartQuestService.
+-- Boot script for SmartQuestService.
 
 local SmartQuestService = require(script.Parent:WaitForChild("SmartQuestService"))
 
+-- Optional integration examples:
+-- SmartQuestService:SetRewardHandler(function(player, rewards, quest, state)
+-- 	-- Connect to CurrencyService, InventoryService, XPService, BadgeService, etc.
+-- end)
+--
+-- SmartQuestService:SetPrerequisiteHandler("HasItem", function(player, itemId, quest)
+-- 	-- return InventoryService:HasItem(player, itemId), "You need " .. itemId
+-- end)
+--
+-- SmartQuestService:SetConditionHandler("HasItem", function(player, itemId, quest, step)
+-- 	-- return InventoryService:HasItem(player, itemId), "You need " .. itemId
+-- end)
+
 SmartQuestService:Init()
 
-print("[SmartQuestService] SmartQuestService initialized.")
+print("[SmartQuestService] v2 initialized.")
